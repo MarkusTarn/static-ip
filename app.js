9189b507d7c23dfd621254ca655e67d602915ef9
Markus Tarn <Markus.Tarn@myjar.com>
2020-01-27 17:31:13 +0200

    Added express

--- app.js
+++ app.js
@@ -1,34 +1,24 @@
-const http = require('http'),
-const config = require('./configuration')
+const { port } = require('./configuration').service
+const express = require('express')
+const app = express()
+
 const dnsMap = {}
 
-http.createServer((req, resp) => {
-    console.log('method', req.method)
-    console.log('dnsMap', dnsMap)
-    console.log('client', req.url)
-    switch (req.method) {
-        case 'GET':
-            const client = req.url
-            if (client in dnsMap) {
-                resp.writeHead(200, { 'Content-Type': 'application/json' })
-                resp.write(JSON.stringify({ client: dnsMap.client }));
-                resp.end();
-            } else {
-                resp.writeHead(404, 'Resource not found', { 'Content-Type': 'application/json' })
-                resp.write(JSON.stringify({ client: 'Resource not found' }));
-                resp.end();
-            }
-            break;
-        case 'PUT':
-            let reqBody = '';
-            req.on('data', data => reqBody += data)
-            req.on('end', () => updateDNS(req))
-            break;
+const getDNS = (req, res) => {
+    const { client } = req.params
+    if (client in dnsMap) {
+        res.status(200).json({ client: dnsMap[client] })
+    } else {
+        res.status(404).json({ client: 'Resource not found' })
     }
-}).listen(config.service.port, () => console.log(`Api server started on port ${config.service.port}`))
-
-function updateDNS(req) {
-    dnsMap[req.url] = req.header('x-forwarded-for') || req.connection.remoteAddress
-    resp.writeHead(200, { 'Content-Type': 'application/json' })
-    resp.end();
 }
+const updateDNS = (req, res) => {
+    const { client } = req.params
+    dnsMap[client] = req.header('x-forwarded-for') || req.connection.remoteAddress
+    res.status(200).json({ client: dnsMap[client] })
+}
+
+app.get('/:client', getDNS)
+app.put('/:client', updateDNS)
+
+app.listen(port, () => console.log(`Service listening on port ${port}!`))
