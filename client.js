8ccaa7018a3e35366c47217cb42cdf448fba934f
Markus Tarn <markustarn@gmail.com>
2020-01-28 23:34:46 +0200

    Update client.js

--- client.js
+++ client.js
@@ -1,11 +1,11 @@
 const { request } = require('https');
 
 const conf = {
-	host: 'www.example.com', // Server that is hosting ip addresses and forwarding requests
-	port: 443,				 // 80 or 443 depending whether server is running on http or https
-	path: '/myhome',		 // Client resource name, your ip and port will be mapped to that key
-	applicationPort: 1337,   // Port to forward domain to
-	updateInterval: 1800000, // Interval to update ip (30 min)
+	host: 'www.example.com',     // Server that is hosting ip addresses and forwarding requests
+	port: 443,                   // 80 or 443 depending whether server is running on http or https
+	path: '/myhome',             // Client resource name, your ip and port will be mapped to that key
+	applicationPort: 1337,       // Port to forward domain to
+	updateInterval: 1800000,     // Interval to update ip (30 min)
 };
 
 const payload = JSON.stringify({ port: conf.applicationPort })
@@ -23,8 +23,8 @@ const options = {
 
 function pingIP() {
 	const req = request(options, res => console.log(`statusCode: ${res.statusCode}`));
-    req.on('error', error => console.error(error));
-    req.write(payload);
+	req.on('error', error => console.error(error));
+	req.write(payload);
 	req.end();
 	setTimeout(() => pingIP(), conf.updateInterval);
 }
