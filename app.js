d98577abd4b25d3daf1617425fb2b517bb002024
Markus Tarn <Markus.Tarn@myjar.com>
2020-01-28 12:21:32 +0200

    Code cleanup

--- app.js
+++ app.js
@@ -4,21 +4,17 @@ const app = express()
 
 const dnsMap = {}
 
-const getDNS = (req, res) => {
+app.get('/:client', function (req, res) {
     const { client } = req.params
-    if (client in dnsMap) {
-        res.status(200).json({ client: dnsMap[client] })
-    } else {
-        res.status(404).json({ client: 'Resource not found' })
-    }
-}
-const updateDNS = (req, res) => {
+    console.log('Client queried: ', client)
+    if (client in dnsMap) res.status(200).json({ client: dnsMap[client] })
+    else res.status(404).json({ client: 'Resource not found' })
+})
+
+app.put('/:client', function (req, res) {
     const { client } = req.params
     dnsMap[client] = req.header('x-forwarded-for') || req.connection.remoteAddress
     res.status(200).json({ client: dnsMap[client] })
-}
-
-app.get('/:client', getDNS)
-app.put('/:client', updateDNS)
+})
 
 app.listen(port, () => console.log(`Service listening on port ${port}!`))
